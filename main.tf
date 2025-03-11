a97e64e2fdfc0b798f76d56652b323fc2466ca11
Astrid Stokkeland <astrid.stokkeland@soprasteria.com>
2025-03-11 11:06:56 +0100

    add appgw

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ resource "azurerm_resource_group" "default" {
   location = "westeurope"
 }
 
-resource "azurerm_virtual_network" "default" {
-  name                = "vnet1"
+resource "azurerm_virtual_network" "adme" {
+  name                = "vnet_adme"
   resource_group_name = azurerm_resource_group.default.name
   location            = azurerm_resource_group.default.location
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "default" {
-    name                 = "subnet1"
+resource "azurerm_subnet" "adme" {
+    name                 = "subnet_adme"
     resource_group_name  = azurerm_resource_group.default.name
-    virtual_network_name = azurerm_virtual_network.default.name
+    virtual_network_name = azurerm_virtual_network.adme.name
     address_prefixes     = ["10.0.1.0/24"]
   
 }
@@ -61,3 +61,103 @@ resource "azurerm_resource_group_template_deployment" "example" {
 
   template_content    = file("template.json")
   }
+
+
+
+
+
+resource "azurerm_virtual_network" "appgw" {
+  name                = "vn_appgw"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  address_space       = ["10.2.0.0/16"]
+}
+
+resource "azurerm_subnet" "appgw" {
+  name                 = "subnet_appgw"
+  resource_group_name  = azurerm_resource_group.default.name
+  virtual_network_name = azurerm_virtual_network.appgw.name
+  address_prefixes     = ["10.2.0.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-pip"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+  allocation_method   = "Static"
+}
+
+# since these variables are re-used - a locals block makes this more maintainable
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.appgw.name}-beap"
+  frontend_port_name             = "${azurerm_virtual_network.appgw.name}-feport"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.appgw.name}-feip"
+  http_setting_name              = "${azurerm_virtual_network.appgw.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.appgw.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.appgw.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.appgw.name}-rdrcfg"
+}
+
+resource "azurerm_application_gateway" "network" {
+  name                = "example-appgw"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = azurerm_resource_group.default.location
+
+  sku {
+    name     = "WAF_v2"
+    tier     = "WAF_v2"
+    capacity = 2
+  }
+
+  waf_configuration {
+    enabled = true
+    firewall_mode = "Prevention"
+    rule_set_type = "OWASP"
+    rule_set_version = "3.0"
+  }
+  
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.appgw.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 9
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+}
